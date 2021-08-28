f42d969ba93ddfe5440600a84cc6b4697c8b5508
Duan Ryan <duanyuluo@gmail.com>
2021-08-28 19:13:29 +0800

    4rd ver

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 # encoding=utf-8
 
-age_mommy = 38
-age_daddy = 43
-age_max = 14
+age_all_members = [38, 43, 14, 70, 68, 74, 66, 1]
+height_all_members = [170, 178, 130, 164, 174, 180, 169, 15]
 
-height_mommy = 170
-height_daddy = 178
-height_max = 130
+def sum_of(all_data):
+    result_data = 0
+    for data in all_data:
+        result_data = result_data + data
+    return result_data
 
-def sum_of(a, b, c):
-    return (a+b+c)
+def avg_of(all_data):
+    return sum_of(all_data) / len(all_data)
 
-def avg_of(a, b, c):
-    return sum_of(a, b, c) / 3
+print("Total age of my family is ", sum_of(age_all_members))
+print("Avg age of my family is", avg_of(age_all_members))
 
-print("Total age of my family is ", sum_of(age_mommy, age_daddy, age_max))
-print("Avg age of my family is", avg_of(age_mommy, age_daddy, age_max))
-
-print("Total height of my family is ", sum_of(height_mommy, height_daddy, height_max))
-print("Avg height of my family is", avg_of(height_mommy, height_daddy, height_max))
+print("Total height of my family is ", sum_of(height_all_members))
+print("Avg height of my family is", avg_of(height_all_members))
