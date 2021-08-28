7036153508ab29441b10a6603039275c3956295a
Duan Ryan <duanyuluo@gmail.com>
2021-08-28 19:07:59 +0800

    3rd finish

--- main.py
+++ main.py
@@ -4,5 +4,18 @@ age_mommy = 38
 age_daddy = 43
 age_max = 14
 
-print("Total age of my family is ", age_mommy + age_daddy + age_max)
-print("Avg age of my family is", (age_mommy + age_daddy + age_max) / 3)
+height_mommy = 170
+height_daddy = 178
+height_max = 130
+
+def sum_of(a, b, c):
+    return (a+b+c)
+
+def avg_of(a, b, c):
+    return sum_of(a, b, c) / 3
+
+print("Total age of my family is ", sum_of(age_mommy, age_daddy, age_max))
+print("Avg age of my family is", avg_of(age_mommy, age_daddy, age_max))
+
+print("Total height of my family is ", sum_of(height_mommy, height_daddy, height_max))
+print("Avg height of my family is", avg_of(height_mommy, height_daddy, height_max))
