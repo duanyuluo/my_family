d32de81fd417ec8a3090739c1521d956c87b472f
Duan Ryan <duanyuluo@gmail.com>
2021-08-28 18:57:31 +0800

    2nd ver: use var to replace constant

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # encoding=utf-8
 
-print("Total age of my family is ", 40+14+43)
-print("Avg age of my family is", (40+14+43) / 3)
+age_mommy = 38
+age_daddy = 43
+age_max = 14
+
+print("Total age of my family is ", age_mommy + age_daddy + age_max)
+print("Avg age of my family is", (age_mommy + age_daddy + age_max) / 3)
